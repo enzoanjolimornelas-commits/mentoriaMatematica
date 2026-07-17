dff003ecdaf71947b67fa088e95c89c5b6336d54
enzoanjolimornelas-commits <enzoanjolimornelas@gmail.com>
2026-07-17 18:37:00 -0300

    API ViaCEP para preencher campos automaticamente baseado no CEP

--- script.js
+++ script.js
@@ -2,6 +2,40 @@ const saibaMais = document.getElementById('saiba-mais');
 const sobre = document.getElementById('sobre');
 
 saibaMais.addEventListener('click', () => {
-    sobre.style.display = 'block';
-    saibaMais.textContent = "Vagas Limitadas!";
+    
+    if(sobre.style.display === 'none'){
+        sobre.style.display = 'block';
+        saibaMais.textContent = "Vagas Limitadas!";
+    } else {
+        sobre.style.display = 'none';
+        saibaMais.textContent = "Saiba Mais"
+    }
+
+});
+
+const cepInput = document.getElementById('cep');
+const cidadeInput = document.getElementById('cidade');
+const ufInput = document.getElementById('uf');
+
+cepInput.addEventListener('blur', async () => {
+    let cep = cepInput.value.replace(/\D/g, '');
+
+    if(cep.length === 8) {
+        try {
+            const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+            const data = await response.json();
+
+            if(!data.error) {
+                cidadeInput.value = data.localidade;
+                ufInput.value = data.uf;
+            } else {
+                alert("CEP não encontrado!");
+                cidadeInput.value = '';
+                ufInput.value = '';
+            }
+        } catch (error) {
+            console.error("Erro ao buscar o CEP:", error);
+            alert("Erro na conexão com o ViaCEP.");
+        }
+    }
 });
